d6929eb62e2c25e6cc1ccb737c20895c14cc58ac
Nadson Sousa <nadsonbcs95@gmail.com>
2025-09-03 12:19:50 -0300

    fix: correct responsive screen

--- src/pages/NotFound/404.tsx
+++ src/pages/NotFound/404.tsx
@@ -1,11 +1,14 @@
+import styles from './404.module.css';
+import { Link } from 'react-router-dom';
 
 export function NotFound() {
-
   return (
-    <>
-    <h1>Página não existe</h1>
-    </>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.message}>Moeda não encontrada.</p>
+      <Link to="/" className={styles.homeButton}>
+        Voltar para a Home
+      </Link>
+    </div>
+  );
 }
-
-
